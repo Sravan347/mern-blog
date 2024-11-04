06789a9e7c17803379624c99e7230b53a541928a
sravan ks <sravansreyas3@gmail.com.com>
2024-11-04 19:49:22 +0530

    connect to the Database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 const express=require('express')
+const { mongoose } = require('mongoose')
+const dotenv = require('dotenv')
+
 const app = express()
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("database is connected");
+    
+}).catch((err)=>{
+    console.log(err);
+    
+
+})
 
 
 const port=5000
@@ -7,3 +21,4 @@ app.listen(port,()=>{
     console.log(`server is running on  port${port} `);
     
 })
+//mernBlog
